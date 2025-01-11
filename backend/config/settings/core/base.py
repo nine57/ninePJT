0d367638fbf9690c1740200ec724ccabfe463686
nine57 <gusrn015@gmail.com>
2025-01-12 00:49:47 +0900

    backend 세팅 수정

--- backend/config/settings/core/base.py
+++ backend/config/settings/core/base.py
@@ -16,7 +16,7 @@ CORS_ORIGIN_WHITELIST = [
     # "https://nine-pjt.info",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "*",
+    "http://127.0.0.1",
     # "nine-pjt.info",
     # "https://nine-pjt.info",
 ]
--- backend/manage.py
+++ backend/manage.py
@@ -3,6 +3,9 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
     """Run administrative tasks."""
