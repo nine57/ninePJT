96df8f192a3dc8b92bac1c77f753f2ce28656dc5
nine57 <gusrn015@gmail.com>
2025-03-07 00:43:59 +0900

    back: accounts directory 처리 및 signup view

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -1,11 +1,9 @@
+from apps.accounts.views import LoginView, SignupView
 from django.urls import path
-
-from apps.accounts.views import LoginRedirectView, LoginView, SignupView
 
 app_name = "accounts"
 
 urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
-    path("redirect/", LoginRedirectView.as_view(), name="redirect"),
 ]
--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,24 +1,34 @@
-from apps.accounts.service import create_user
 from django.contrib.auth import authenticate
-from django.shortcuts import redirect
-from django.urls import reverse
+from django.contrib.auth.models import User
 from django.utils.decorators import method_decorator
-from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import TemplateView
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
-class SignupView(TemplateView):
-    template_name = "accounts/signup.html"
+@method_decorator(csrf_exempt, name="dispatch")
+class SignupView(APIView):
+    permission_classes = [AllowAny]  # 모든 사용자에게 허용
 
-    def post(self, request, *args, **kwargs):
-        request_body = self.request.POST
-        create_user(request_body)
-        return redirect("accounts:login")
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        if User.objects.filter(username=username).exists():
+            return Response(
+                {"error": "사용자가 이미 존재합니다."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        user = User(username=username)
+        user.set_password(password)  # 비밀번호 해싱
+        user.save()
+        return Response(
+            {"message": "회원가입이 완료되었습니다."}, status=status.HTTP_201_CREATED
+        )
 
 
 @method_decorator(csrf_exempt, name="dispatch")
@@ -42,9 +52,3 @@ class LoginView(APIView):
                 {"error": "잘못된 인증 정보입니다."},
                 status=status.HTTP_401_UNAUTHORIZED,
             )
-
-
-# TODO: make logics
-class LoginRedirectView(View):
-    def get(self, request, *args, **kwargs):
-        return redirect(reverse("index"))
